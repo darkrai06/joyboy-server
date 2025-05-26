74b046c76fd4b7fd0a0f976b3241de2872bc77d9
Al_Mahin <mmalmahin@gmail.com>
2025-05-26 18:24:13 +0600

    dtrbrgt

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: '*', 
+  origin: ['http://localhost:5173'],
   credentials: true,
 }));
+
 app.use(express.json());
 // app.use(cookieParser());
 app.use(morgan('dev'))
